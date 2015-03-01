192fd14454e8999bcbb593d17cce8c27d2e8692f
t1238142000 <damody@users.noreply.github.com>
2015-03-01 16:58:30 +0000

    for the first time in forever

---
+++ Source/AVG/AVGGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "AVGGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AVG_API AAVGGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
