47d023d629771d0967194969d5bd952dddbdad0c
t1238142000@gmail.com <t1238142000@gmail.com@80904814-2a12-702e-c28d-59b8e353540d>
2015-06-28 12:23:17 +0000

    update 4.8.0

--- trunk/Plugins/AVG/Source/AVG/Classes/FightCharacter.h
+++ trunk/Plugins/AVG/Source/AVG/Classes/FightCharacter.h
@@ -4,25 +4,81 @@
 
 #include "PaperFlipbook.h"
 #include "PaperCharacter.h"
+#include "PaperFlipbookComponent.h"
 #include "FightCharacter.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class AVG_API AFightCharacter : public APaperCharacter
+class AVG_API AFightCharacter : public ACharacter
 {
-	GENERATED_BODY()
+	GENERATED_UCLASS_BODY()
 	
+	// Name of the Sprite component
+	static FName SpriteComponentName;
+
 public:
+	virtual void PostInitializeComponents() override;
+
+	/** Returns Sprite subobject **/
+	FORCEINLINE class UPaperFlipbookComponent* GetSprite() const { return Sprite; }
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightRole")
+	int32 Faction;
+
+	UPROPERTY(Category = Character, VisibleAnywhere, BlueprintReadOnly)
+	UPaperFlipbookComponent* Sprite;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightRole")
-	UPaperFlipbook*	PF_Walking;
+	UPaperFlipbook* PF_Walking1;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightRole")
-	UPaperFlipbook*	PF_Attacking;
+	UPaperFlipbook* PF_Attacking1;
 
-	float WalkingDuration();
-	
-	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightRole")
+	UPaperFlipbook* PF_Walking2;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightRole")
+	UPaperFlipbook* PF_Attacking2;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightRole")
+	UPaperFlipbook* PF_Walking3;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightRole")
+	UPaperFlipbook* PF_Attacking3;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightRole")
+	int32	RoleLevel;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightRole")
+	int32	Credit;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightRole")
+	float	BasisHP;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightRole")
+	float	HP;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightRole")
+	float	BasisAttackDelay;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightRole")
+	float	AttackDelay;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightRole")
+	int32	AttackRadius;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightRole")
+	int32	BasisAttack;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightRole")
+	int32	Attack;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightRole")
+	int32	BasisMoveSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightRole")
+	int32	MoveSpeed;
+
 };
